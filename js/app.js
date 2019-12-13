275216f1556936cb5477057b2890104ea2cc0e93
maryamrmz <maryam.rpour@gmail.com>
2019-12-14 00:46:21 +0330

    add js file to project

---
+++ js/app.js
@@ -0,0 +1,78 @@
+function getNumber(e) {
+    var showRes = document.getElementById("input");
+    showRes.value == "";
+
+    switch (e) {
+        case 1:
+            showRes.value += 1;
+            break;
+        case 2:
+            showRes.value += 2;
+            break;
+        case 3:
+            showRes.value += 3;
+            break;
+        case 4:
+            showRes.value += 4;
+            break;
+        case 5:
+            showRes.value += 5;
+            break;
+        case 6:
+            showRes.value += 6;
+            break;
+        case 7:
+            showRes.value += 7;
+            break;
+        case 8:
+            showRes.value += 8;
+            break;
+        case 9:
+            showRes.value += 9;
+            break;
+        case 0:
+            showRes.value += 0;
+            break;
+    }
+}
+
+function clearScreen() {
+    document.getElementById("input").value = 0;
+    document.getElementById("input").value = "";
+}
+
+function getOperation(operation) {
+    var showRes = document.getElementById("input");
+    switch (operation) {
+        case "+":
+            showRes.value += " + ";
+            break;
+        case "-":
+            showRes.value += " - ";
+            break;
+        case "*":
+            showRes.value += " * ";
+            break;
+        case "/":
+            showRes.value += " / ";
+            break;
+        case "+/-":
+            showRes.value += " - " + showRes.value;
+            break;
+    }
+}
+
+function complete() {
+    var showRes = document.getElementById("input"),
+        answer = Math.floor(+eval(showRes.value));
+    showRes.value = "= " + answer;
+}
+
+function showNav() {
+    document.querySelector(".navigation").style.display = "block";
+    document.querySelector(".navigation").style.marginTop = "20px";
+}
+
+function hideNav() {
+    document.querySelector(".navigation").style.display = "none";
+}
